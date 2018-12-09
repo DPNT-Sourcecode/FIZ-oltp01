aa347581a77db42ae3c973419f1de49f1647dd3b
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-12-09 07:15:07 +0000

    Sun Dec 09 07:15:07 GMT 2018

---
+++ src/BeFaster.App/Solutions/FIZ/FizzBuzzSolution.cs
@@ -0,0 +1,74 @@
+﻿using BeFaster.Runner.Exceptions;
+
+namespace BeFaster.App.Solutions.FIZ
+{
+    public static class FizzBuzzSolution
+    {
+        public static string FizzBuzz(int number)
+        {
+
+            string output= "";
+
+
+            //if (number % 15 == 0) 
+            //{
+            //    output = "fizz buzz";   
+            //}
+            //else if (number % 3 == 0)
+            //{
+            //    output = "fizz";
+            //}
+
+            //else if (number % 5 == 0)
+            //{
+            //    output = "buzz";
+            //}
+
+            //else 
+            //{
+            //    output = number.ToString();
+            //}
+            //return output;
+
+            string textTest = number.ToString();
+
+            if (number % 15 == 0 || (textTest.Contains("3")) && (textTest.Contains("5")))
+            {
+                output = "fizz buzz";
+            }
+
+            else 
+            {
+                if (number % 3 == 0 || textTest.Contains("3"))
+                {
+                    output = "fizz";
+
+                    if (textTest.Contains("5"))
+                    {
+                        output = "fizz buzz";
+                    }
+                }
+
+                else if (number % 5 == 0 || textTest.Contains("5"))
+                {
+                    output = "buzz";
+
+                    if (textTest.Contains("3"))
+                    {
+                        output = "fizz buzz";
+                    }
+                }
+
+                else
+                {
+                    output = number.ToString();
+                }   
+            }
+
+            return output;
+
+
+            // throw new SolutionNotImplementedException();
+        }
+    }
+}
